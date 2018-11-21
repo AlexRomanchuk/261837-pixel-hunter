75c46cbb252728b6a3c76adc6c5a22e606cbbba0
Alexander <borom@inbox.ru>
2018-11-21 12:39:53 +0300

    Реализованы остальные функции

--- js/util.js
+++ js/util.js
@@ -1,11 +1,50 @@
 // утилиты
 export const Game = {
+  LEVEL: 0,
+  LIVES: 3,
+  TIME: 30,
   COUNT_QUESTIONS: 10,
   IDEAL_STEP: 100,
   BONUS_STEP: 50,
   FAST_ANSWER_TIME: 10,
   SLOW_ANSWER_TIME: 20,
   MIN_ANSWERS: 7
+};
+
+export const removeLives = (lives, answer) => {
+  let timeoutAnswer = (answer.time > Game.TIME) ? true : false;
+  let gamerLives = lives;
+  if (timeoutAnswer) {
+    gamerLives -= 1;
+  }
+  return gamerLives;
+};
+
+export const gameTimer = (time, callback) => {
+  try {
+    let lastTime = time;
+    callback(lastTime);
+    gameTimer.timerInterval = setInterval(() => {
+      callback(lastTime - 1);
+      lastTime--;
+      if (lastTime === -1) {
+        gameTimer.pause();
+        callback(`Time Out!`);
+      }
+    }, 1000);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+gameTimer.pause = () => {
+  clearInterval(gameTimer.timerInterval);
+};
+
+gameTimer.start = (time, callback) => {
+  const result = gameTimer(time, callback);
+  return result;
 };
 
 export const countScore = (answers, lives) => {
@@ -30,6 +69,16 @@ export const countScore = (answers, lives) => {
     score += liveScore;
   }
   return score;
+};
+
+export const changeLevels = (level, answers, callbackGame, callbackScore) => {
+  let curLevel = level;
+  const result = {
+    lives: callbackGame(answers),
+    level: curLevel + 1,
+    score: callbackScore(answers, callbackGame(answers))
+  };
+  return result;
 };
 
 export const showScreen = (elem) => {
